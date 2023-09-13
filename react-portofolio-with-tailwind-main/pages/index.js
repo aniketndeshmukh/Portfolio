83ea6ee7137fc42981d657aa59ca027ab898e698
aniket <aniketdeshmukh7798@gmail.com>
2023-09-13 15:01:08 +0530

    2.1

--- react-portofolio-with-tailwind-main/pages/index.js
+++ react-portofolio-with-tailwind-main/pages/index.js
@@ -44,13 +44,13 @@ export default function Home() {
       <main className=" bg-white dark:bg-gray-900 md:px-20 lg:px-40 ">
         <section className="min-h-screen ">
           <nav className="py-10 mb-12 flex justify-between dark:text-white bg-transparent z-20 shadow-md "  >
-            <a href="/#"><h1 className="font-burtons text-xl " >Aniket Deshmukh</h1></a>
+            <link href="/#"><h1 className="font-burtons text-xl " >Aniket Deshmukh</h1></link>
             <ul className="flex items-center lm:right-11">
               <li>
-                <a href="#portfolio"><h2  className=" px-4 py-2 ">Portfolio</h2></a>
+                <link href="#portfolio"><h2  className=" px-4 py-2 ">Portfolio</h2></link>
               </li>
               <li>
-                <a href="#skills"><h2  className="sm:px-4 sm:py-1 lm:px-2 lm:py-1  ">Skills</h2></a>
+                <link href="#skills"><h2  className="sm:px-4 sm:py-1 lm:px-2 lm:py-1  ">Skills</h2></link>
               </li>
               <li>
                 <BsFillMoonStarsFill
@@ -59,19 +59,19 @@ export default function Home() {
                 />
               </li>
               <li>
-                <a
+                <link
                   className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white sm:ml-4 sm:px-4 sm:py-2 lm:px-2 lm:py-1  border-none rounded-md sm:ml-2 lm:ml-6  "
                   href="/Resume.pdf" download>
                   Resume
-                </a>
+                </link>
               </li>
               <li>
-                <a
+                <link
                   className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white sm:ml-9 sm:px-2 sm:py-2 lm:px-2 lm:py-1 border-none rounded-md sm:ml-2 lm:ml-6"
                   href="mailto:aniketdeshmukh019@gmail.com"
                 >
                   ContactMe
-                </a>
+                </link>
               </li>
             </ul>
             
@@ -84,12 +84,12 @@ export default function Home() {
               Java Full Stack Developer & Web Developer.
             </h3>
             <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
-            To succeed in an environment of growth and excellence and earn a job which provides me job satisfaction and self development and help me achieve personal as well as organizational goals.
+            To succeed in an environment of growth and excellence and earn link job which provides me job satisfaction and self development and help me achieve personal as well as organizational goals.
             </p></motion.div>
             <motion.div initial={{opacity:0,y:-100}} animate={{opacity:1,y:0}} transition={{delay:0.3}}><div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
-              <a href="https://github.com/aniketndeshmukh"><AiFillGithub /></a>
-              <a href="https://www.linkedin.com/in/aniket-deshmukh-64386521b/"><AiFillLinkedin /></a>
-             <a href="https://www.instagram.com/aniketndeshmukh"> <AiFillInstagram /></a>
+              <link href="https://github.com/aniketndeshmukh"><AiFillGithub /></link>
+              <link href="https://www.linkedin.com/in/aniket-deshmukh-64386521b/"><AiFillLinkedin /></link>
+             <link href="https://www.instagram.com/aniketndeshmukh"> <AiFillInstagram /></link>
             </div>
             <div  className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96 ">
               <Image src={deved} layout="fill" objectFit="cover" />
@@ -101,8 +101,8 @@ export default function Home() {
           <div>
             <h3 className="text-3xl py-1 dark:text-white ">Skills</h3>
             <p className="text-md gap-y-0.5 text-lg font-semibold py-2 leading-8 text-gray-800 dark:text-gray-200">
-              Since the beginning of my journey as a Software developer and Web
-              developer, I have learnt a lot about new technologies like for Java Developement
+              Since the beginning of my journey as link Software developer and Web
+              developer, I have learnt link lot about new technologies like for Java Developement
               <span className="text-cyan-50"> Spring Boot </span>
               and for Web Developement <span className="text-cyan-50">React Js </span>
               and My hunger for learning new technologies is growing. 
@@ -118,7 +118,7 @@ export default function Home() {
                 Front End 
               </h3>
               <p className="py-2">
-              Possess a solid grasp of HTML tags and CSS, along with practical experience in developing applications using React.js.
+              Possess link solid grasp of HTML tags and CSS, along with practical experience in developing applications using React.js.
               </p>
               <h4 className="py-4 text-teal-600">I have good knowledge in following technologies</h4>
               <p className=" text-gray-50 dark:text-gray-800 py-1">HTML</p>
@@ -133,7 +133,7 @@ export default function Home() {
                 Back End 
               </h3>
               <p className="py-2">
-              Possess a strong grasp of logical programming, Java development, and object-oriented programming with expertise in Java and Spring Boot.
+              Possess link strong grasp of logical programming, Java development, and object-oriented programming with expertise in Java and Spring Boot.
               </p>
               <h4 className="py-4 text-teal-600">I'm good in following Backend technologies</h4>
               <p className=" text-gray-50 dark:text-gray-800  py-1">JAVA</p>
@@ -160,7 +160,7 @@ export default function Home() {
           <div>
             <h3 className="text-3xl py-1 dark:text-white ">Portofolio</h3>
             <p className="text-md py-2 font-semibold leading-8 text-gray-800 dark:text-gray-200">
-            My portfolio showcases a diverse range of software projects developed using Java, J2EE, Spring Boot, Hibernate, HTML, and CSS. These projects highlight my expertise in building robust and scalable web applications, leveraging Java-based technologies for backend development and creating responsive, user-friendly front-end interfaces. With a strong foundation in software engineering, I have successfully implemented solutions that demonstrate proficiency in core Java programming, efficient database management with Hibernate, and seamless integration of the Spring Boot framework. Each project reflects my commitment to delivering high-quality software that meets both functional and aesthetic standards.
+            My portfolio showcases link diverse range of software projects developed using Java, J2EE, Spring Boot, Hibernate, HTML, and CSS. These projects highlight my expertise in building robust and scalable web applications, leveraging Java-based technologies for backend development and creating responsive, user-friendly front-end interfaces. With link strong foundation in software engineering, I have successfully implemented solutions that demonstrate proficiency in core Java programming, efficient database management with Hibernate, and seamless integration of the Spring Boot framework. Each project reflects my commitment to delivering high-quality software that meets both functional and aesthetic standards.
               
             </p>
             <p className="text-md py-2 leading-8 font-semibold text-cyan-500  ">
@@ -182,7 +182,7 @@ export default function Home() {
               Javafx. The Proposed System Reduces Hotel Management Effort Because Of Integration of
               Order and Billing System. This System Takes Hotel Service to Digital Level Which Makes Hotel
               Management Less Human Dependent.</p>
-             <div className="mr-auto ml-auto w-32"> <a href="https://github.com/aniketndeshmukh/RestaurantManagementSystem" className="bg-gradient-to-r mt-5  from-cyan-500 text- to-teal-500  sm:ml-4 sm:px-4 sm:py-2 lm:px-2 lm:py-1 border-none rounded-md sm:ml-2 lm:ml-6 flex w-32 "><AiFillGithub className="lm:w-7 lm:h-7 sm:w-8 sm:h-12"></AiFillGithub><span className="ml-3 font-bold mt-1"> Git Hub</span></a></div>
+             <div className="mr-auto ml-auto w-32"> <link href="https://github.com/aniketndeshmukh/RestaurantManagementSystem" className="bg-gradient-to-r mt-5  from-cyan-500 text- to-teal-500  sm:ml-4 sm:px-4 sm:py-2 lm:px-2 lm:py-1 border-none rounded-md sm:ml-2 lm:ml-6 flex w-32 "><AiFillGithub className="lm:w-7 lm:h-7 sm:w-8 sm:h-12"></AiFillGithub><span className="ml-3 font-bold mt-1"> Git Hub</span></link></div>
             </div>
            <div className="basis-1/3 text-center shadow-lg p-10 rounded-xl  bg-gray-800 text-gray-50 dark:text-gray-800 dark:bg-white flex-1" ><h1 className="text-xl font-bold">BOOK STORE MANAGEMENT SYSTEM</h1><div className=" block ml-auto mr-auto w-56" >
               
@@ -196,7 +196,7 @@ export default function Home() {
               can keep record of all the books in his Store. With the help of this System, Seller can Manage
               all the book records. In this Project I have used java language and for front end I have used
               HTML,CSS,BOOTSTRAP and JavaScript. For back End I have used SpringBoot.</p>
-             <div className="mr-auto ml-auto w-32"> <a href="#" className="bg-gradient-to-r mt-5  from-cyan-500 text- to-teal-500  sm:ml-4 sm:px-4 sm:py-2 lm:px-2 lm:py-1 border-none rounded-md sm:ml-2 lm:ml-6 flex  w-32 "><AiFillGithub className="lm:w-7 lm:h-7 sm:w-8 sm:h-12"></AiFillGithub><span className="ml-3 font-bold mt-1"> Git Hub</span></a></div>
+             <div className="mr-auto ml-auto w-32"> <link href="#" className="bg-gradient-to-r mt-5  from-cyan-500 text- to-teal-500  sm:ml-4 sm:px-4 sm:py-2 lm:px-2 lm:py-1 border-none rounded-md sm:ml-2 lm:ml-6 flex  w-32 "><AiFillGithub className="lm:w-7 lm:h-7 sm:w-8 sm:h-12"></AiFillGithub><span className="ml-3 font-bold mt-1"> Git Hub</span></link></div>
             </div>
             <div className=" lm:w-1/2 lm:flex-initial mr-auto ml-auto text-center shadow-lg p-10 rounded-xl   bg-gray-800 text-gray-50 dark:text-gray-800 dark:bg-white " >
               <h1 className="text-xl font-bold"> ONLINE PERSONAL PORTFOLIO</h1><div className=" block ml-auto mr-auto w-56" >
@@ -209,17 +209,17 @@ export default function Home() {
                 src={web3}
               /></div> <p className="mt-3 font-semibold">I've built an online portfolio showcasing my projects and skills. 
               I used HTML, CSS, Tailwind CSS, React JS, Next JS, and Node JS to create this website.
-               It's a platform where I present my work and highlight my expertise in web development and design.</p>
+               It's link platform where I present my work and highlight my expertise in web development and design.</p>
              <div className="flex "><div className="mr-auto ml-auto w-32"> 
-             <a href="https://github.com/aniketndeshmukh/Aniket-Portfolio/tree/main/react-portofolio-with-tailwind-main" className="bg-gradient-to-r mt-5  from-cyan-500 text- to-teal-500 
+             <link href="https://github.com/aniketndeshmukh/Aniket-Portfolio/tree/main/react-portofolio-with-tailwind-main" className="bg-gradient-to-r mt-5  from-cyan-500 text- to-teal-500 
               sm:ml-1 sm:px-4 sm:py-2 lm:px-2 lm:py-1 border-none rounded-md lm:ml-1 flex w-32 ">
                 <AiFillGithub className="lm:w-7 lm:h-7 sm:w-8 sm:h-12">
-                  </AiFillGithub><span className="ml-3 font-bold mt-1"> Git Hub</span></a></div> <div className="mr-auto ml-auto w-36"> 
-             <a href="#" className="bg-gradient-to-r mt-5  from-cyan-500 text- to-teal-500 
+                  </AiFillGithub><span className="ml-3 font-bold mt-1"> Git Hub</span></link></div> <div className="mr-auto ml-auto w-36"> 
+             <link href="#" className="bg-gradient-to-r mt-5  from-cyan-500 text- to-teal-500 
               sm:ml-1 sm:px-4 sm:py-2 lm:px-2 lm:py-1 border-none rounded-md  lm:ml-1 flex w-36 ">
                 
                 <FaLink  className="lm:w-7 lm:h-7 sm:w-8 sm:h-12"></FaLink>
-                  <span className="ml-3 font-bold mt-1">Live Demo</span></a></div> 
+                  <span className="ml-3 font-bold mt-1">Live Demo</span></link></div> 
                   
                   </div>
             </div>
@@ -227,7 +227,7 @@ export default function Home() {
         </section>
       </main>
       <footer style={{width:'100%'}} className="h-60 p-20 bg-teal-600">
-       <div  className=" relative mx-auto  flex w-fit  footmenu top-4  text-2xl "> <a href="#portfolio" className="pr-7 pt-1"><h3>Portfolio</h3></a> <a href="#skills" className="pr-3 pt-1" ><h3>Skills</h3></a > <a   href="mailto:aniketdeshmukh019@gmail.com" className="pr-2 pt-1"><AiFillMail className=" w-7 h-7"></AiFillMail></a> <a  href="https://www.linkedin.com/in/aniket-deshmukh-64386521b/" className="pr-2  pt-1"><AiFillLinkedin className="w-7 h-7"></AiFillLinkedin></a><a href="https://github.com/aniketndeshmukh" className="pr-2  pt-1"><AiFillGithub className="w-7 h-7 "></AiFillGithub></a></div> 
+       <div  className=" relative mx-auto  flex w-fit  footmenu top-4  text-2xl "> <link href="#portfolio" className="pr-7 pt-1"><h3>Portfolio</h3></link> <link href="#skills" className="pr-3 pt-1" ><h3>Skills</h3></link > <link   href="mailto:aniketdeshmukh019@gmail.com" className="pr-2 pt-1"><AiFillMail className=" w-7 h-7"></AiFillMail></link> <link  href="https://www.linkedin.com/in/aniket-deshmukh-64386521b/" className="pr-2  pt-1"><AiFillLinkedin className="w-7 h-7"></AiFillLinkedin></link><link href="https://github.com/aniketndeshmukh" className="pr-2  pt-1"><AiFillGithub className="w-7 h-7 "></AiFillGithub></link></div> 
        <div style={{height:'2px',width:'100%'}} className=" w-screen relative  bg-gray-800 top-7"></div>
      <div className="relative top-16 flex"><FaRegCopyright className=" h-5 w-5"/><h1>AniketDeshmukh.All Rights Are Reserved.</h1></div>
       </footer>
